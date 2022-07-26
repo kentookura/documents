f9fbbdc2195af39a1f1adf25c2f546cfe6ba80db
Kento Okura <kento@okura.at>
2022-07-26 22:54:31 +0200

    move to nix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+          inherit (pkgs.texlive) scheme-full latex-bin latexmk;
       };
     in rec {
       packages = {
@@ -22,12 +22,14 @@
             export PATH="${pkgs.lib.makeBinPath buildInputs}";
             mkdir -p .cache/texmf-var
             env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              SOURCE_DATE_EPOCH=${toString self.lastModified} \
               latexmk -interaction=nonstopmode -pdf -lualatex \
-              main.tex
+              pretex="\pdfvariable suppressoptionalinfo 512\relax" \
+              -usepretex main.tex
           '';
           installPhase = ''
             mkdir -p $out
-            cp document.pdf $out/Okura_Bsc.pdf
+            cp main.pdf $out/Okura_Bsc.pdf
           '';
         };
       };
