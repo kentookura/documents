c2ababe9999174e8ae7729be9c034bf972b3c300
Kento Okura <kento@okura.at>
2022-09-11 14:00:17 +0200

    Get rid of all latexrun warnings
    Start final sprint. Started cleaning up and finalizing section on
    etale morphisms. Start working on sheaf semantics.

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = [
     pkgs.hello
+    pkgs.latexrun
 
     # keep this line if you use bash
     pkgs.bashInteractive
